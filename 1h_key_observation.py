81b3b1d7fc8424f9b7ceb4ffb1f836fd586cd30f
Anshuman kumar <anshuman.kumar1@renewbuy.com>
2025-08-22 17:56:44 +0530

    changes

---
+++ 1h_key_observation.py
@@ -0,0 +1,80 @@
+'''
+                                                   1H DATA   KEY PATTERN OBSERVATIONS                           
+
+'''
+
+'''
+--------------------------------------------------------------------------------------------------------------
+   Confirmed observation  and strategy
+--------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------------
+'''
+
+
+
+
+
+
+'''
+--------------------------------------------------------------------------------------------------------------
+Self analysed Key observations :
+
+Low Volatility: < 0.005              Expect small, range-bound moves. Good for scalping
+High Volatility: > 0.01              Expect larger price swings. Good for breakout strategies 
+Very High Volatility: > 0.015        High risk/reward
+
+
+Simple Rule-Based Approach:
+Enter trades when volatility is rising from low to medium (e.g., vol jumps from 0.004 to 0.008).
+Exit or reduce size when volatility is very high (>0.015), as unpredictability increases
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------------
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+------------------------------------------------------------------------
+Key Thresholds for actionable signals
+Volatility Spike:
+If garman_klass_vol > 0.01, expect a large move (>2000 pips) next hour.
+Extreme Move:
+If |Pips_Moved| > 5000, expect a reversal (50-70% of the time).
+Time Window:
+Trade between 17:00 - 20:00 IST for highest activity.
+------------------------------------------------------------------------
+
+'''
